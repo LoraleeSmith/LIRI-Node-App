83bc0aee8a0765146fe30856121bc9654ee11831
Loralee Dawn <50155935+LoraleeSmith@users.noreply.github.com>
2019-11-23 15:59:57 -0800

    updated files
    redoing the liri.js and keys.js files

--- liri.js
+++ liri.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-
 var spotify = new Spotify(keys.spotify);
 
-node liri.js  concert - this < artist / band name >
-    node liri.js  spotify - this < song >
-        node liri.js  movie - this < title >
-            node liri.js  do - this < whatever told >
+node liri.js concert - this < "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp" >
+
+    node liri.js spotify - this - song < "https://rest.spotify.com/song" + title + "/app_id=d1692f05e78349748043632bf1011d6f" >
+
+
+        node liri.js movie - this < "https://rest.omdb.com/movie" + title + "app_id=trilogy" >
+
+
+            node liri.js do -what - it - says
